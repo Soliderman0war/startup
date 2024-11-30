738e803c4510f43deb3647d438397fb16a7154ca
Soliderman0war <drakeamueller@gmail.com>
2024-11-30 16:31:37 -0700

    trying to move index and a htmls over

--- 15minutes/src/App.jsx
+++ 15minutes/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ 15minutes/src/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Activity Page Stub</h1>;
+  };
+
+export default Home;
