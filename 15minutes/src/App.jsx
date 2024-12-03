b842afec2fd49aa9fe9771d7985fd289132bad5e
Soliderman0war <drakeamueller@gmail.com>
2024-12-02 19:05:14 -0700

    update

--- 15minutes/src/App.jsx
+++ 15minutes/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from './Home';
+import Login from './Login';
 
 const App = () => {
   return (
--- 15minutes/src/Login.jsx
+++ 15minutes/src/Login.jsx
@@ -1,6 +1,12 @@
 const Login = () => {
-    return <h1>Login Page</h1>;
-  };
-
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <input type="text" placeholder="Enter your username" />
+      <input type="password" placeholder="Enter your password" />
+      <button>Login</button>
+    </div>
+  );
+};
 
 export default Login;
